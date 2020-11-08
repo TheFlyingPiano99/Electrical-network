fa86946f14eafec0130d6d1ea3401983dd093cd4
TheFlyingPiano99 <57034327+TheFlyingPiano99@users.noreply.github.com>
2020-11-08 21:40:38 +0100

    init2

---
+++ src/network/Component.java
@@ -0,0 +1,5 @@
+package network;
+
+public abstract class Component {
+
+}
---
+++ src/network/Main.java
@@ -0,0 +1,10 @@
+package network;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Electrical networks");
+	}
+	// TODO Auto-generated method stub
+
+}
