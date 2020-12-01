4f58623718c75efdac21fd0d4f7a0bbbda528aaf
TheFlyingPiano99 <57034327+TheFlyingPiano99@users.noreply.github.com>
2020-12-01 12:19:41 +0100

    comments in vector and matrix

--- src/main/java/math/Matrix.java
+++ src/main/java/math/Matrix.java
@@ -12,6 +12,44 @@ public class Matrix {
     public int row;
     public int column;
 
+
+	//Constructors:---------------------------------------------------
+	
+    /**
+     * Constructor
+     * @param r - number of rows
+     * @param c - number of columns
+     * @return
+     */
+    public Matrix(int r,int c) {
+    	row = r;
+    	column = c;
+        n = new float[row * column];
+        /*if (n == nullptr) {
+            throw runtime_error("Initialisation failed!");
+        }*/
+    }
+
+    /**
+     * Constructor
+     * @param M source matrix
+     */
+    public Matrix(Matrix M) {
+    	row = M.row;
+    	column = M.column;
+        n = new float[row * column];
+/*        if (n == nullptr) {
+            throw std::runtime_error("Initialisation failed!");
+        }*/
+        for (int c = 0; c < column; c++) {
+            for  (int r = 0; r < row; r++) {
+                n[c * row + r] = M.at(r, c);
+            }
+        }
+    }
+
+    //hashCode/equals:---------------------------------------------------
+    
     @Override
 	public int hashCode() {
 		final int prime = 31;
@@ -44,55 +82,31 @@ public class Matrix {
 		return true;
 	}
 
+    //Indexing:-------------------------------------------------------
+    
     /**
-     * Constructor
-     * @param r - number of rows
-     * @param c - number of columns
-     * @return
-     */
-    public Matrix(int r,int c) {
-    	row = r;
-    	column = c;
-        n = new float[row * column];
-        /*if (n == nullptr) {
-            throw runtime_error("Initialisation failed!");
-        }*/
-    }
-
-    /**
-     * 
-     * @param M source matrix
-     */
-    public Matrix(Matrix M) {
-    	row = M.row;
-    	column = M.column;
-        n = new float[row * column];
-/*        if (n == nullptr) {
-            throw std::runtime_error("Initialisation failed!");
-        }*/
-        for (int c = 0; c < column; c++) {
-            for  (int r = 0; r < row; r++) {
-                n[c * row + r] = M.at(r, c);
-            }
-        }
-    }
-
-    /**
-     * Indexing
+     * Indexing. Returns the value stored at position of r. row and c. column. 
      * @param r - row index
      * @param c - columns index
-     * @return
+     * @return	value
      */
     public float at(int r, int c) { //indexeles (sor, oszlop)
     	return n[c * row + r];
     }
     
+    /**
+     * Writing. Sets the value at position of r. row and c. column to the given value. 
+     * @param r	row index
+     * @param c column index
+     * @param val new value at given position.
+     */
     public void setAt(int r, int c, float val) {
     	n[c * row + r] = val;
     }
 
     /**
      * Fills up matrix with given parameter
+     * HUN: Mátrix feltöltése a kapott értékkel.
      * @param val - to fill with
      */
     public void fill(float val) {
--- src/main/java/math/Vector.java
+++ src/main/java/math/Vector.java
@@ -2,7 +2,8 @@ package main.java.math;
 
 
 /**
- * 
+ * Vector of float values.
+ * HUN: Vektor lebegőpontos értékek tárolására.
  * @author Simon Zoltán
  *
  */
@@ -10,6 +11,24 @@ public class Vector {
 	private float n[];
     public int dimension;
 
+
+	//Constructors:-------------------------------------------------
+	
+    public Vector(int d) {
+    	dimension = d;
+        n = new float[dimension];
+    }
+
+    public Vector(Vector v) {
+    	dimension = v.dimension; 
+        n = new float[dimension];
+        for (int i = 0; i < dimension; i++) {
+            n[i] = v.at(i);
+        }
+    }
+
+    //Override of default hashCode and equals:---------------------------------------
+    
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -37,33 +56,44 @@ public class Vector {
 		return true;
 	}
 
-    public Vector(int d) {
-    	dimension = d;
-        n = new float[dimension];
-    }
-
-    public Vector(Vector v) {
-    	dimension = v.dimension; 
-        n = new float[dimension];
-        for (int i = 0; i < dimension; i++) {
-            n[i] = v.at(i);
-        }
-    }
-
-    public float at(int i) {
+	//Indexing-methods:-----------------------------------------------------
+	
+	/**
+     * Indexing. Returns the value stored at position i. 
+	 * @param i index
+	 * @return value
+	 */
+	public float at(int i) {
         return n[i];
     }
     
+	/**
+     * Writing. Sets the value at position i to the given value. 
+	 * @param i index
+	 * @param val new value
+	 */
 	public void setAt(int i, float val) {
 		n[i] = val;		
 	}
 	
+	/**
+     * Fills up vector with given parameter.
+     * HUN: Vektor feltöltése a kapott értékkel.
+	 * @param val
+	 */
     public void fill (float val) {
         for (int i = 0; i < dimension; i++) {
             n[i] = val;
         }
     }
 
+    //Operations:------------------------------------------------------
+
+    /**
+     *  Multiply this vector by scalar s.
+     * @param s scalar float value to multiply with.
+     * @return this
+     */
     public Vector multiply(float s) {
         for (int i = 0; i < dimension; i++) {
             n[i] *= s;
@@ -71,6 +101,11 @@ public class Vector {
         return this;
     }
     
+    /**
+     *  Divide this vector by scalar s.
+     * @param s scalar float value to divide by.
+     * @return this
+     */
     public Vector divide(float s) {
         s = 1.0F / s;
         for (int i = 0; i < dimension; i++) {
@@ -79,6 +114,11 @@ public class Vector {
         return this;
     }
 
+    /**
+     *  Add vector v to this vector.
+     * @param v vector to add.
+     * @return this
+     */
     public Vector add(Vector v) {
         for (int i = 0; i < dimension; i++) {
             n[i] += v.at(i);
@@ -86,13 +126,23 @@ public class Vector {
         return this;
     }
     
-    public Vector subtrackt(Vector v) {
+    /**
+     *  Subtract vector v from this vector.
+     * @param v vector to subtract.
+     * @return this
+     */
+    public Vector subtract(Vector v) {
         for (int i = 0; i < dimension; i++) {
             n[i] -= v.at(i);
         }
         return this;
     }
     
+    /**
+     * Copy content from vector v to this vector.
+     * @param v source vector.
+     * @return this
+     */
     public Vector copy(Vector v) {
         if (!this.equals(v)) {
             if (this.dimension != v.dimension) {
